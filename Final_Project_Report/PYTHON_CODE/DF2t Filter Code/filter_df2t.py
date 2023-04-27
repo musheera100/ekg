f63d99c1e30185c8d9d9312700ad4f9dbb331c44
musheera100 <75345079+musheera100@users.noreply.github.com>
2023-04-26 23:48:10 -0400

    Update filter_df2t.py

--- Final_Project_Report/PYTHON_CODE/DF2t Filter Code/filter_df2t.py
+++ Final_Project_Report/PYTHON_CODE/DF2t Filter Code/filter_df2t.py
@@ -19,6 +19,29 @@ def filter_df2t(b, a, x):
             shift_reg_v[j] = shift_reg_v[j-1] + x[i]*b[k-j-1] - y[i]*a[k-j-1]
         shift_reg_v[0] = x[i]*b[k-1] - y[i]*a[k-1]
     return y
+def sma(x,y,k):
+    y = [0] * (k-1)
+    for i in range(k-1, len(x)):
+        window = x[i-k+1:i+1]
+        y.append(sum(window) / k)
+
+
+def arryth(x,p,tc,th,c,h):
+    c = 0
+    h = 0
+    for i in range(len(x)):
+        if(p[i]):
+            if (x[i] > th):
+                h+=1
+            if(x[i]<tc):
+                c+=1
+
+#brute force finding location of peaks
+def peaks(x,p):
+    p = [-1]*len(x)
+    for i in range(len(x)):
+        if (x[i]>10 and x[i]>x[i-1] and x[i]>x[i+1]):
+            p[i] = 1
 
 # Testing Direct Form II Filter Function
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -40,8 +63,28 @@ b = [0.000000495352235,0.000002972113413,0.000007430283531,0.000009907044708,
 a = [1, -5.271918566723349, 11.619927717930022, -13.702695926669762,
      9.116066297447906, -3.243419845060293, 0.482072025618542]
 
+b_hp = [0.998431665916719,-0.998431665916719]
+a_hp = [1.000000000000000,-0.996863331833438]
+
 # Filtering the signal
-ecg_filt = filter_df2t(b, a, ecg_sig)
+ecg_lp = filter_df2t(b_lp, a_lp, ecg_sig)
+ecg_filt = filter_df2t(b_hp,a_hp,ecg_lp)
+
+ECG_sq = [i**2 for i in ecg_filt]
+k = 30
+ECG_sma = [0]*len(ECG_sq)
+sma(ecg_filt,ECG_sma,k)
+p = [-1]*len(ECG_sq)
+peaks(ECG_sq,p)
+tc = 1.5
+th = 1.5
+c=0
+h=0
+arryth(ECG_sq,p,tc,th,c,h)
+if(c>30):
+    print("conduction disturbance detected")
+if(h>0): 
+    print("ST/T Change Detected")
 
 # Plotting the filtered and unfiltered waveforms
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(12, 3))
